657e5c03d0b1839a9740db0c372d0baeb98ed166
liangchaofei <chaofei_liang@126.com>
2021-04-07 09:18:37 +0800

    feat: 检查用户主目录

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -3,6 +3,8 @@
 module.exports = core;
 
 const semver = require('semver');
+const userHome = require('user-home');
+const pathExists = require('path-exists');
 const colors = require('colors/safe');
 const pkg = require('../package.json')
 const log = require('@my-template-cli/log')
@@ -13,6 +15,7 @@ function core() {
         checkPkgVersion()
         checkNodeVersion()
         checktRoot()
+        checkUserHome()
     }catch(error){
         log.error(error.message)
     }
@@ -40,4 +43,11 @@ function checktRoot(){
     const rootCheck = require('root-check');
     rootCheck()
     console.log(process.geteuid()) 
+}
+
+// 检查用户主目录
+function checkUserHome(){
+    if(!userHome || !pathExists){
+        throw new Error(colors.red('当前登录用户主目录不存在!'))
+    }
 }
