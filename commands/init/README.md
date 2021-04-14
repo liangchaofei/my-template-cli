3011ec52ef7d758440a1b4dde7bcfec6701286e6
liangchaofei <chaofei_liang@126.com>
2021-04-14 09:21:34 +0800

    feat: init初始化

---
+++ commands/init/README.md
@@ -0,0 +1,11 @@
+# `@my-template-cli/init`
+
+> TODO: description
+
+## Usage
+
+```
+const init = require('@my-template-cli/init');
+
+// TODO: DEMONSTRATE API
+```
---
+++ commands/init/lib/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+
+function init(projectName, cmdObj) {
+    // TODO
+    console.log('aa')
+    console.log('init', projectName, cmdObj.force)
+}
+module.exports = init;
