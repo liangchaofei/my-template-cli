3f09b463dbd1e5a3e4bec4f197f219e38012d56d
liangchaofei <chaofei_liang@126.com>
2021-04-20 09:01:02 +0800

    feat: 获取本地入口文件

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,14 +1,24 @@
 'use strict';
 const Package = require('@my-template-cli/package')
+const log = require('@my-template-cli/log')
 
+const SETTINGS = {
+    init: '@@my-template-cli/init'
+}
 
 function exec() {
-    // TODO
-    const pkg = new Package();
+    const targetPath = process.env.CLI_TARGET_PATH;
+    const homePath = process.env.CLI_HOME_PATH;
+    const cmdObj = arguments[arguments.length - 1];
+    const cmdName = cmdObj.name(); // 获取 init这个名称
+    const packageName = SETTINGS[cmdName];
+    const packageVersion = 'latest'
+    const pkg = new Package({
+        targetPath,
+        packageName,
+        packageVersion
+    });
     console.log(pkg)
-    console.log('exec')
-    console.log('exec_target_path',process.env.CLI_TARGET_PATH)
-    console.log(process.env.CLI_HOME_PATH)
 }
 
 module.exports = exec;
