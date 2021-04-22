c0d2a6f7b44808080f8f71369f2bf52145b63266
liangchaofei <chaofei_liang@126.com>
2021-04-22 20:41:52 +0800

    feat: 调整

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -15,7 +15,7 @@ async function exec() {
     const cmdObj = arguments[arguments.length - 1];
     const cmdName = cmdObj.name(); // 获取 init这个名称
     const packageName = SETTINGS[cmdName];
-    const packageVersion = '1.0.0';
+    const packageVersion = 'latest';
 
     if(!targetPath){
         targetPath = path.resolve(homePath, CACHE_DIR) // 生成缓存路径
