f221f0be98236a4655c71b23f5d1eaa19d1d9a23
liangchaofei <chaofei_liang@126.com>
2021-04-23 08:49:09 +0800

    feat: 通用command

--- models/command/lib/index.js
+++ models/command/lib/index.js
@@ -2,15 +2,31 @@
 
 const semver = require('semver');
 const colors = require('colors/safe');
+const log = require('@my-template-cli/log');
 const LOWEST_NODE_VERSION = '12.0.0';
 
 class Command{
     constructor(argv){
         console.log('command',argv)
+        if(!argv){
+            throw new Error('参数不能为空')
+        }
+        if(!Array.isArray(argv)){
+            throw new Error('参数必须为数组')
+        }
+        if(argv.length < 1){
+            throw new Error('参数列表为空')
+        }
         this._argv = argv;
         let runner = new Promise((resolve,reject) => {
             let chain = Promise.resolve();
             chain = chain.then(() => this.checkNodeVersion())
+            chain = chain.then(() => this.initArgs())
+            chain = chain.then(() => this.init())
+            chain = chain.then(() => this.exec())
+            chain.catch(err =>{
+                log.error(err.message)
+            })
         })
     }
     // 检查node版本号
@@ -25,6 +41,11 @@ class Command{
         }
     }
 
+    // 初始化参数
+    initArgs(){
+        this.cmd = this._argv[this._argv.length - 1];
+        this._argv = this._argv.slice(0,this._argv.length-1)
+    }
     init(){
         throw new Error('init必须实现')
     }
